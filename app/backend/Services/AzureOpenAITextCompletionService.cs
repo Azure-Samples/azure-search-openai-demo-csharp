8a51f295cbaf70a718c7e776259ab929315dd0cf
Devis Lucato <dluc@users.noreply.github.com>
2023-04-06 14:23:04 -0700

    Initial PR: azure-search-openai-demo with dotnet backend (#2)
    Initial PR: azure-search-openai-demo with dotnet backend
    
    ---------
    
    Co-authored-by: Liam Cavanagh <liamca@users.noreply.github.com>
    Co-authored-by: Microsoft Open Source <microsoftopensource@users.noreply.github.com>
    Co-authored-by: Pablo Castro <pablocas@microsoft.com>
    Co-authored-by: Arun Chandrasekhar <14188700+achandmsft@users.noreply.github.com>
    Co-authored-by: Alex Zeltov <azeltov@gmail.com>
    Co-authored-by: Sunil Sattiraju <sunilsattiraju@gmail.com>
    Co-authored-by: Jon Gallant <2163001+jongio@users.noreply.github.com>
    Co-authored-by: Victor Vazquez <vhvb1989@gmail.com>
    Co-authored-by: anatolip <anatolip@microsoft.com>
    Co-authored-by: mkmsftgit <94026703+mkmsftgit@users.noreply.github.com>
    Co-authored-by: XiaoYun Zhang <xiaoyuz@microsoft.com>
    Co-authored-by: Xiaoyun Zhang <bigmiao.zhang@gmail.com>
    Co-authored-by: David Pine <david.pine@microsoft.com>
    Co-authored-by: David Fowler <davidfowl@gmail.com>

---
+++ app/backend/Services/AzureOpenAITextCompletionService.cs
@@ -0,0 +1,42 @@
+﻿namespace Services;
+
+using Azure.AI.OpenAI;
+using Microsoft.SemanticKernel.AI;
+using Microsoft.SemanticKernel.AI.TextCompletion;
+
+internal sealed class AzureOpenAITextCompletionService : ITextCompletion
+{
+    private readonly OpenAIClient _openAIClient;
+    private readonly string _deployedModelName;
+
+    public AzureOpenAITextCompletionService(OpenAIClient openAIClient, string deployedModelName)
+    {
+        _openAIClient = openAIClient;
+        _deployedModelName = deployedModelName;
+    }
+
+    public async Task<string> CompleteAsync(string text, CompleteRequestSettings requestSettings, CancellationToken cancellationToken = default)
+    {
+        var option = new CompletionsOptions();
+        option.Prompts.Add(text);
+        option.Temperature = Convert.ToSingle(requestSettings.Temperature);
+        foreach (var stopSequence in requestSettings.StopSequences)
+        {
+            option.StopSequences.Add(stopSequence);
+        }
+
+        option.MaxTokens = requestSettings.MaxTokens;
+        option.FrequencyPenalty = Convert.ToSingle(requestSettings.FrequencyPenalty);
+        option.PresencePenalty = Convert.ToSingle(requestSettings.PresencePenalty);
+
+        var response = await _openAIClient.GetCompletionsAsync(_deployedModelName, option, cancellationToken);
+        if (response.Value is Completions completions && completions.Choices.Count >= 1)
+        {
+            return completions.Choices.First().Text;
+        }
+        else
+        {
+            throw new AIException(AIException.ErrorCodes.InvalidConfiguration, "completion not found");
+        }
+    }
+}
