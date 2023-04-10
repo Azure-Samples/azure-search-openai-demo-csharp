d6428e484ae30c603beea247392a061fe5761e5f
XiaoYun Zhang <xiaoyuz@microsoft.com>
2023-04-10 11:11:13 -0700

    update start.sh by removing python installing command

--- app/start.sh
+++ app/start.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# Absolute path to this script, e.g. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f "$0")
+# Absolute path this script is in, thus /home/user/bin
+SCRIPTPATH=$(dirname "$SCRIPT")
+
 echo ""
 echo "Loading azd .env file from current environment"
 echo ""
@@ -16,25 +21,11 @@ if [ $? -ne 0 ]; then
     exit $?
 fi
 
-echo 'Creating python virtual environment "backend/backend_env"'
-python -m venv backend/backend_env
-
-echo ""
-echo "Restoring backend python packages"
-echo ""
-
-cd backend
-./backend_env/bin/python -m pip install -r requirements.txt
-if [ $? -ne 0 ]; then
-    echo "Failed to restore backend python packages"
-    exit $?
-fi
-
 echo ""
 echo "Restoring frontend npm packages"
 echo ""
 
-cd ../frontend
+cd $SCRIPTPATH/frontend
 npm install
 if [ $? -ne 0 ]; then
     echo "Failed to restore frontend npm packages"
@@ -55,9 +46,8 @@ echo ""
 echo "Starting backend"
 echo ""
 
-cd ../backend
-xdg-open http://127.0.0.1:5000
-./backend_env/bin/python ./app.py
+cd $SCRIPTPATH/backend
+dotnet run
 if [ $? -ne 0 ]; then
     echo "Failed to start backend"
     exit $?
