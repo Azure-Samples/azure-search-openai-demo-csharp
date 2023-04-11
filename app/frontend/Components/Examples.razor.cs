aaad9559beb2f6c2603dfcf2e24543d500e56f13
David Pine <david.pine@microsoft.com>
2023-04-11 10:42:06 -0500

    More updates and additions for the examples, etc.

---
+++ app/frontend/Components/Examples.razor.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+namespace ClientApp.Components;
+
+public sealed partial class Examples
+{
+    [Inject] public required IStringLocalizer<Examples> Localizer { get; set; }
+
+    private string WhatsIncluded => Localizer[nameof(WhatsIncluded)];
+    private string WhatsPerfReview => Localizer[nameof(WhatsPerfReview)];
+    private string WhatsProductManagerDo => Localizer[nameof(WhatsProductManagerDo)];
+}
--- app/frontend/Pages/Ask.razor.cs
+++ app/frontend/Pages/Ask.razor.cs
@@ -4,4 +4,7 @@ namespace ClientApp.Pages;
 
 public sealed partial class Ask
 {
+    private string _userPrompt = "";
+    private bool _isReceivingResponse = false;
+    private string? _intermediateResponse = null;
 }
