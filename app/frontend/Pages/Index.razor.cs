0d26eddb92e21f6a66cf6816d5f9893328b90df4
David Pine <david.pine.7@gmail.com>
2023-04-24 10:38:22 -0500

    Address concerns with generated images

--- app/frontend/Pages/Index.razor.cs
+++ app/frontend/Pages/Index.razor.cs
@@ -4,7 +4,20 @@ namespace ClientApp.Pages;
 
 public sealed partial class Index
 {
-    private readonly string[] _images = Enumerable.Range(0, 10)
-        .Select(i => $"media/bing-generated-{i}.jpg")
-        .ToArray();
+    private readonly Image[] _images = new Image[]
+    {
+        new("media/bing-generated-0.jpg", "The birth of generative AI, color pop"),
+        new("media/bing-generated-1.jpg", ".NET, pop art"),
+        new("media/bing-generated-2.jpg", "Computers evolving beyond humanity retro futurism"),
+        new("media/bing-generated-3.jpg", "Futuristic robots playing chess, Banksy art, color splash"),
+        new("media/bing-generated-4.jpg", "Artificial intelligence, geometric art, bright and vibrant"),
+        new("media/bing-generated-5.jpg", "Robot made of analog stereo equipment, digital art"),
+        new("media/bing-generated-6.jpg", "Steaming cup of coffee with text on side, pop art"),
+        new("media/bing-generated-7.jpg", "Futuristic scene with skyscrapers, hovercrafts and robots"),
+        new("media/bing-generated-8.jpg", "Robots playing chess, Banksy art"),
+        new("media/bing-generated-9.jpg", "Artificial intelligence, geometric art, bright and vivid"),
+        new("media/bing-generated-10.jpg", "Old 1950s computer on pick background, retro futurism")
+    };
 }
+
+internal readonly record struct Image(string Src, string Alt);
