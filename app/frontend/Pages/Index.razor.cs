cfaf59b953835c6c309cda74e7a72aec427695e6
David Pine <david.pine@microsoft.com>
2023-04-24 12:20:45 -0500

    Update Image Creator generated images to not include faces (#42)
    Address concerns with generated images, contributes to #40.

--- app/frontend/Pages/Index.razor.cs
+++ app/frontend/Pages/Index.razor.cs
@@ -4,7 +4,20 @@ namespace ClientApp.Pages;
 
 public sealed partial class Index
 {
-    private readonly string[] _images = Enumerable.Range(0, 10)
-        .Select(i => $"media/bing-generated-{i}.jpg")
-        .ToArray();
+    private readonly Image[] _images = new Image[]
+    {
+        new("media/bing-create-0.jpg", "The birth of generative AI, color pop"),
+        new("media/bing-create-1.jpg", ".NET, pop art"),
+        new("media/bing-create-2.jpg", "Computers evolving beyond humanity retro futurism"),
+        new("media/bing-create-3.jpg", "Futuristic robots playing chess, Banksy art, color splash"),
+        new("media/bing-create-4.jpg", "Artificial intelligence, geometric art, bright and vibrant"),
+        new("media/bing-create-5.jpg", "Robot made of analog stereo equipment, digital art"),
+        new("media/bing-create-6.jpg", "Steaming cup of coffee with text on side, pop art"),
+        new("media/bing-create-7.jpg", "Futuristic scene with skyscrapers, hovercrafts and robots"),
+        new("media/bing-create-8.jpg", "Robots playing chess, Banksy art"),
+        new("media/bing-create-9.jpg", "Artificial intelligence, geometric art, bright and vivid"),
+        new("media/bing-create-10.jpg", "Old 1950s computer on pick background, retro futurism")
+    };
 }
+
+internal readonly record struct Image(string Src, string Alt);
