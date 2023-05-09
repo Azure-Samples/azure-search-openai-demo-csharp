97abefd8ad972190e9d55fe7992c1d25c68316f0
Xiaoyun Zhang <bigmiao.zhang@gmail.com>
2023-05-09 10:37:46 -0700

    remove e.g examples prompt in chat service (#74)
    fix
    https://github.com/Azure-Samples/azure-search-openai-demo-csharp/issues/16
    ## Does this introduce a breaking change?
    <!-- Mark one with an "x". -->
    ```
    [ ] Yes
    [ ] No
    ```
    
    ## Pull Request Type
    What kind of change does this Pull Request introduce?
    
    <!-- Please check the one that applies to this PR using "x". -->
    ```
    [ ] Bugfix
    [ ] Feature
    [ ] Code style update (formatting, local variables)
    [ ] Refactoring (no functional changes, no api changes)
    [ ] Documentation content changes
    [ ] Other... Please describe:
    ```
    
    ## How to Test
    *  Get the code
    
    ```
    git clone [repo-address]
    cd [repo-name]
    git checkout [branch-name]
    npm install
    ```
    
    * Test the code
    <!-- Add steps to run the tests suite and/or manually test -->
    ```
    ```
    
    ## What to Check
    Verify that the following are valid
    * ...
    
    ## Other Information
    <!-- Add any other helpful information that may be needed here. -->

--- app/backend/Services/ReadRetrieveReadChatService.cs
+++ app/backend/Services/ReadRetrieveReadChatService.cs
@@ -17,10 +17,10 @@ public class ReadRetrieveReadChatService
 
     private const string AnswerPromptTemplate = """
         <|im_start|>
-        system Assistant helps the company employees with their healthcare plan questions, and questions about the employee handbook. Be brief in your answers.
+        You are a system assistant who helps the company employees with their healthcare plan questions, and questions about the employee handbook. Be brief in your answers.
         Answer ONLY with the facts listed in the list of sources below. If there isn't enough information below, say you don't know. Do not generate answers that don't use the sources below. If asking a clarifying question to the user would help, ask the question.
         For tabular information return it as an html table. Do not return markdown format.
-        Each source has a name followed by colon and the actual information, always include the source name for each fact you use in the response. Use square brakets to reference the source, e.g. [info1.txt]. Don't combine sources, list each source separately, e.g. [info1.txt][info2.pdf].
+        Each source has a name followed by colon and the actual information, always include the source file for each fact you use in the response. Use square brakets to reference the source. Don't combine sources, list each source separately.
         {{$follow_up_questions_prompt}}
         {{$injected_prompt}}
         Sources:
