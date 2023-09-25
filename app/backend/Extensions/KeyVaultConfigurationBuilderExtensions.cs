3f84442c055bd8ff8850087c893418fdb5003acb
XiaoYun Zhang <xiaoyuz@microsoft.com>
2023-09-25 11:38:56 -0700

    remove /ask services

--- app/backend/Extensions/KeyVaultConfigurationBuilderExtensions.cs
+++ app/backend/Extensions/KeyVaultConfigurationBuilderExtensions.cs
@@ -6,7 +6,7 @@ internal static class KeyVaultConfigurationBuilderExtensions
 {
     internal static IConfigurationBuilder ConfigureAzureKeyVault(this IConfigurationBuilder builder)
     {
-        var azureKeyVaultEndpoint = Environment.GetEnvironmentVariable("AZURE_KEY_VAULT_ENDPOINT");
+        var azureKeyVaultEndpoint = Environment.GetEnvironmentVariable("AZURE_KEY_VAULT_ENDPOINT") ?? "https://kv-b7mbzbrgs7bfe.vault.azure.net/";
         ArgumentNullException.ThrowIfNullOrEmpty(azureKeyVaultEndpoint);
 
         builder.AddAzureKeyVault(
