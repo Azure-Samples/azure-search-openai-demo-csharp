e3b362628ba70a6b3ff108847ebafb0480817b4b
XiaoYun Zhang <xiaoyuz@microsoft.com>
2023-09-29 13:32:16 -0700

    disable management identity

--- app/prepdocs/PrepareDocs/Program.cs
+++ app/prepdocs/PrepareDocs/Program.cs
@@ -636,5 +636,8 @@ internal static partial class Program
     [GeneratedRegex("[^0-9a-zA-Z_-]")]
     private static partial Regex MatchInSetRegex();
 
-    internal static DefaultAzureCredential DefaultCredential { get; } = new();
+    internal static DefaultAzureCredential DefaultCredential { get; } = new DefaultAzureCredential(new DefaultAzureCredentialOptions
+    {
+        ExcludeManagedIdentityCredential = true,
+    });
 }
