a47ced95ac6d21e2f122caea595d0c113c6ccb8f
Jon Gallant <2163001+jongio@users.noreply.github.com>
2023-11-01 11:21:07 -0700

    Add func (#209)
    ## Purpose
    <!-- Describe the intention of the changes being proposed. What problem
    does it solve or functionality does it add? -->
    * ...
    
    ## Does this introduce a breaking change?
    <!-- Mark one with an "x". -->
    ```
    [ ] Yes
    [ ] No
    ```
    
    ## Pull Request Type
    What kind of change does this Pull Request introduce?
    
    <!-- Please check the one that applies to this PR using "x". -->
    ```
    [ ] Bugfix
    [ ] Feature
    [ ] Code style update (formatting, local variables)
    [ ] Refactoring (no functional changes, no api changes)
    [ ] Documentation content changes
    [ ] Other... Please describe:
    ```
    
    ## How to Test
    *  Get the code
    
    ```
    git clone [repo-address]
    cd [repo-name]
    git checkout [branch-name]
    npm install
    ```
    
    * Test the code
    <!-- Add steps to run the tests suite and/or manually test -->
    ```
    ```
    
    ## What to Check
    Verify that the following are valid
    * ...
    
    ## Other Information
    <!-- Add any other helpful information that may be needed here. -->

--- scripts/prepdocs.ps1
+++ scripts/prepdocs.ps1
@@ -1,35 +1,69 @@
-Write-Host ""
-Write-Host "Loading azd .env file from current environment"
-Write-Host ""
-
-$output = azd env get-values
-
-foreach ($line in $output) {
-    $name, $value = $line.Split("=")
-    $value = $value -replace '^\"|\"$'
-    [Environment]::SetEnvironmentVariable($name, $value)
+if (-not $script:azdCmd) {
+    $script:azdCmd = Get-Command azd -ErrorAction SilentlyContinue
 }
 
-Write-Host "Environment variables set."
+# Check if 'azd' command is available
+if (-not $script:azdCmd) {
+    throw "Error: 'azd' command is not found. Please ensure you have 'azd' installed. For installation instructions, visit: https://learn.microsoft.com/en-us/azure/developer/azure-developer-cli/install-azd"
+}
+
+if (-not $script:dotnetCmd) {
+    $script:dotnetCmd = Get-Command dotnet -ErrorAction SilentlyContinue
+}
+
+# Check if 'dotnet' command is available
+if (-not $script:dotnetCmd) {
+    throw "Error: 'dotnet' command is not found. Please ensure you have 'dotnet' installed. For installation instructions, visit: https://learn.microsoft.com/en-us/dotnet/core/tools/"
+}
+
+function Invoke-ExternalCommand {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$Command,
+        
+        [Parameter(Mandatory = $false)]
+        [string]$Arguments
+    )
+
+    $processStartInfo = New-Object System.Diagnostics.ProcessStartInfo
+    $processStartInfo.FileName = $Command
+    $processStartInfo.Arguments = $Arguments
+    $processStartInfo.RedirectStandardOutput = $true
+    $processStartInfo.RedirectStandardError = $true
+    $processStartInfo.UseShellExecute = $false
+    $processStartInfo.CreateNoWindow = $true
+
+    $process = New-Object System.Diagnostics.Process
+    $process.StartInfo = $processStartInfo
+    $process.Start() | Out-Null
+    $output = $process.StandardOutput.ReadToEnd()
+    $errorOutput = $process.StandardError.ReadToEnd()
+    $process.WaitForExit()
+
+    if ($errorOutput) {
+        Write-Error $errorOutput
+    }
+
+    return $output
+}
 
 if ([string]::IsNullOrEmpty($env:AZD_PREPDOCS_RAN) -or $env:AZD_PREPDOCS_RAN -eq "false") {
     Write-Host 'Running "PrepareDocs.dll"'
 
     Get-Location | Select-Object -ExpandProperty Path
 
-    dotnet run --project "app/prepdocs/PrepareDocs/PrepareDocs.csproj" -- `
-        './data/*.pdf' `
-        --storageendpoint $env:AZURE_STORAGE_BLOB_ENDPOINT `
-        --container $env:AZURE_STORAGE_CONTAINER `
-        --searchendpoint $env:AZURE_SEARCH_SERVICE_ENDPOINT `
-        --searchindex $env:AZURE_SEARCH_INDEX `
-        --openaiendpoint $env:AZURE_OPENAI_ENDPOINT `
-        --embeddingmodel $env:AZURE_OPENAI_EMBEDDING_DEPLOYMENT `
-        --formrecognizerendpoint $env:AZURE_FORMRECOGNIZER_SERVICE_ENDPOINT `
-        --tenantid $env:AZURE_TENANT_ID `
-        -v
+    $dotnetArguments = @"
+    run --project "app/prepdocs/PrepareDocs/PrepareDocs.csproj" "./data/*.pdf" --storageendpoint $($env:AZURE_STORAGE_BLOB_ENDPOINT) --container $($env:AZURE_STORAGE_CONTAINER) --searchendpoint $($env:AZURE_SEARCH_SERVICE_ENDPOINT) --searchindex $($env:AZURE_SEARCH_INDEX) --openaiendpoint $($env:AZURE_OPENAI_ENDPOINT) --embeddingmodel $($env:AZURE_OPENAI_EMBEDDING_DEPLOYMENT) --formrecognizerendpoint $($env:AZURE_FORMRECOGNIZER_SERVICE_ENDPOINT) --tenantid $($env:AZURE_TENANT_ID) --verbose
+"@
+    
+    $output = Invoke-ExternalCommand -Command "dotnet" -Arguments $dotnetArguments
+    Write-Host $output
 
-    azd env set AZD_PREPDOCS_RAN "true"
-} else {
+    Invoke-ExternalCommand -Command ($azdCmd).Source -Arguments @"
+    env set AZD_PREPDOCS_RAN "true"
+"@
+
+}
+else {
     Write-Host "AZD_PREPDOCS_RAN is set to true. Skipping the run."
 }
