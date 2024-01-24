9a0d66bc7da5e0ba93f45ff78a12cf1977cd43d0
Xiaoyun Zhang <bigmiao.zhang@gmail.com>
2024-01-24 12:11:23 -0800

    Add AzureComputerVisionService (#261)
    ## Purpose
    This PR adds AzureComputerVisionService, which will be used to create
    image embedding and generate image embedding from text query.
    
    
    ## Does this introduce a breaking change?
    <!-- Mark one with an "x". -->
    ```
    [ ] Yes
    [x] No
    ```
    
    ## Pull Request Type
    What kind of change does this Pull Request introduce?
    
    #257 
    <!-- Please check the one that applies to this PR using "x". -->
    ```
    [ ] Bugfix
    [x] Feature
    [ ] Code style update (formatting, local variables)
    [ ] Refactoring (no functional changes, no api changes)
    [ ] Documentation content changes
    [ ] Other... Please describe:
    ```
    
    ## How to Test
    *  Get the code
    
    ```
    git clone [repo-address]
    cd [repo-name]
    git checkout [branch-name]
    npm install
    ```
    
    * Test the code
    <!-- Add steps to run the tests suite and/or manually test -->
    ```
    ```
    
    ## What to Check
    Verify that the following are valid
    * ...
    
    ## Other Information
    <!-- Add any other helpful information that may be needed here. -->
    
    ---------
    
    Co-authored-by: David Pine <david.pine@microsoft.com>

---
+++ app/backend/Services/AzureComputerVisionService.cs
@@ -0,0 +1,92 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace MinimalApi.Services;
+
+public class AzureComputerVisionService(IHttpClientFactory httpClientFactory, string endPoint, string apiKey)
+{
+    // add virtual keyword to make it mockable
+    public virtual async Task<ImageEmbeddingResponse> VectorizeImageAsync(string imagePathOrUrl, CancellationToken ct = default)
+    {
+        var api = $"{endPoint}/computervision/retrieval:vectorizeImage?api-version=2023-02-01-preview&modelVersion=latest";
+        // first try to read as local file
+        if (File.Exists(imagePathOrUrl))
+        {
+            using var request = new HttpRequestMessage(HttpMethod.Post, api);
+
+            // set authorization header
+            request.Headers.Add("Ocp-Apim-Subscription-Key", apiKey);
+
+            // set body
+            var bytes = await File.ReadAllBytesAsync(imagePathOrUrl, ct);
+            request.Content = new ByteArrayContent(bytes);
+            request.Content.Headers.ContentType = new MediaTypeHeaderValue("image/*");
+
+            // send request
+            using var client = httpClientFactory.CreateClient();
+            using var response = await client.SendAsync(request, ct);
+            response.EnsureSuccessStatusCode();
+
+            // read response
+            var json = await response.Content.ReadAsStringAsync();
+            var result = JsonSerializer.Deserialize<ImageEmbeddingResponse>(json);
+
+            return result ?? throw new InvalidOperationException("Failed to deserialize response");
+        }
+        else
+        {
+            // retrieve as url
+            using var request = new HttpRequestMessage(HttpMethod.Post, api);
+
+            // set content type to application/json
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            // set authorization header
+            request.Headers.Add("Ocp-Apim-Subscription-Key", apiKey);
+
+            // set body
+            var body = new { url = imagePathOrUrl };
+            request.Content = new StringContent(JsonSerializer.Serialize(body), Encoding.UTF8, "application/json");
+
+            // send request
+            using var client = httpClientFactory.CreateClient();
+            using var response = await client.SendAsync(request, ct);
+            response.EnsureSuccessStatusCode();
+
+            // read response
+            var json = await response.Content.ReadAsStringAsync();
+            var result = JsonSerializer.Deserialize<ImageEmbeddingResponse>(json);
+
+            return result ?? throw new InvalidOperationException("Failed to deserialize response");
+        }
+    }
+
+    public virtual async Task<ImageEmbeddingResponse> VectorizeTextAsync(string text, CancellationToken ct = default)
+    {
+        var api = $"{endPoint}/computervision/retrieval:vectorizeText?api-version=2023-02-01-preview&modelVersion=latest";
+
+        using var request = new HttpRequestMessage(HttpMethod.Post, api);
+
+        // set content type to application/json
+        request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+        // set authorization header
+        request.Headers.Add("Ocp-Apim-Subscription-Key", apiKey);
+
+        // set body
+        var body = new { text };
+        request.Content = new StringContent(JsonSerializer.Serialize(body), Encoding.UTF8, "application/json");
+
+        // send request
+        using var client = new HttpClient();
+        using var response = await client.SendAsync(request, ct);
+        response.EnsureSuccessStatusCode();
+
+        // read response
+        var json = await response.Content.ReadAsStringAsync();
+        var result = JsonSerializer.Deserialize<ImageEmbeddingResponse>(json);
+        return result ?? throw new InvalidOperationException("Failed to deserialize response");
+    }
+}
