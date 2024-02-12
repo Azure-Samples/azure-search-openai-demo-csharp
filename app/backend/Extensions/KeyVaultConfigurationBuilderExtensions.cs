af2438372af11b2f2f24e1b0bab623a4b60ad9f4
XiaoYun Zhang <xiaoyuz@microsoft.com>
2024-02-11 21:32:43 -0800

    update

--- app/backend/Extensions/KeyVaultConfigurationBuilderExtensions.cs
+++ app/backend/Extensions/KeyVaultConfigurationBuilderExtensions.cs
@@ -6,7 +6,7 @@ internal static class KeyVaultConfigurationBuilderExtensions
 {
     internal static IConfigurationBuilder ConfigureAzureKeyVault(this IConfigurationBuilder builder)
     {
-        var azureKeyVaultEndpoint = Environment.GetEnvironmentVariable("AZURE_KEY_VAULT_ENDPOINT") ?? "https://kv-lttiq7bnc65tu.vault.azure.net/";
+        var azureKeyVaultEndpoint = Environment.GetEnvironmentVariable("AZURE_KEY_VAULT_ENDPOINT");
         ArgumentNullException.ThrowIfNullOrEmpty(azureKeyVaultEndpoint);
 
         builder.AddAzureKeyVault(
