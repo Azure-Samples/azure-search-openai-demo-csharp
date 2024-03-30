f2da5ec6b9a1085d3567795fd9e8b4d0f1de1869
Deepakkr15 <165126838+Deepakkr15@users.noreply.github.com>
2024-03-30 14:25:39 +0000

    Pending changes exported from your codespace

--- app/SharedWebComponents/Components/Examples.razor.cs
+++ app/SharedWebComponents/Components/Examples.razor.cs
@@ -7,9 +7,9 @@ public sealed partial class Examples
     [Parameter, EditorRequired] public required string Message { get; set; }
     [Parameter, EditorRequired] public EventCallback<string> OnExampleClicked { get; set; }
 
-    private string WhatIsIncluded { get; } = "What is included in my Northwind Health Plus plan that is not in standard?";
-    private string WhatIsPerfReview { get; } = "What happens in a performance review?";
-    private string WhatDoesPmDo { get; } = "What does a Product Manager do?";
+    private string WhatIsIncluded { get; } = "List the upcoming developments in Atlanta";
+    private string WhatIsPerfReview { get; } = "Show me properties that are 5-bed 4-bath and less than 0.5 million";
+    private string WhatDoesPmDo { get; } = "Share some design ideas for my Kitchen";
 
     private async Task OnClickedAsync(string exampleText)
     {
