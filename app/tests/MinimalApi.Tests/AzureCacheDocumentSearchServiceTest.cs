6247c64a84631a7db8c61ea27db1a262d92982e8
Stanley Small <103065678+stanleysmall-microsoft@users.noreply.github.com>
2024-04-24 08:40:03 -0400

    make tests pass

--- app/tests/MinimalApi.Tests/AzureCacheDocumentSearchServiceTest.cs
+++ app/tests/MinimalApi.Tests/AzureCacheDocumentSearchServiceTest.cs
@@ -8,20 +8,20 @@ using Shared.Models;
 namespace MinimalApi.Tests;
 public class AzureCacheDocumentSearchServiceTest
 {
-    private static readonly string s_acIndex = "gptkbindex";
-    private static readonly string s_openAiEmbeddingDeployment = "embedding";
-    private static readonly string s_cacheEndpoint = "";
-    private static readonly string s_openAiEndpoint = "";
-    private static readonly string s_computerVisionEndpoint = "";
+    //private static readonly string s_acIndex = "gptkbindex";
+    //private static readonly string s_openAiEmbeddingDeployment = "embedding";
+    //private static readonly string s_cacheEndpoint = "";
+    //private static readonly string s_openAiEndpoint = "";
+    //private static readonly string s_computerVisionEndpoint = "";
 
 
-    [Xunit.Fact]
+    [EnvironmentVariablesFact("AZURE_CACHE_INDEX", "AZURE_CACHE_SERVICE_ENDPOINT", "AZURE_OPENAI_ENDPOINT", "AZURE_OPENAI_EMBEDDING_DEPLOYMENT")]
     public async Task QueryDocumentsTestTextOnlyAsync()
     {
-        Environment.SetEnvironmentVariable("AZURE_CACHE_INDEX", s_acIndex);
-        Environment.SetEnvironmentVariable("AZURE_CACHE_SERVICE_ENDPOINT", s_cacheEndpoint);
-        Environment.SetEnvironmentVariable("AZURE_OPENAI_ENDPOINT", s_openAiEndpoint);
-        Environment.SetEnvironmentVariable("AZURE_OPENAI_EMBEDDING_DEPLOYMENT", s_openAiEmbeddingDeployment);
+        //Environment.SetEnvironmentVariable("AZURE_CACHE_INDEX", s_acIndex);
+        //Environment.SetEnvironmentVariable("AZURE_CACHE_SERVICE_ENDPOINT", s_cacheEndpoint);
+        //Environment.SetEnvironmentVariable("AZURE_OPENAI_ENDPOINT", s_openAiEndpoint);
+        //Environment.SetEnvironmentVariable("AZURE_OPENAI_EMBEDDING_DEPLOYMENT", s_openAiEmbeddingDeployment);
 
         var index = Environment.GetEnvironmentVariable("AZURE_CACHE_INDEX") ?? throw new InvalidOperationException();
         var endpoint = Environment.GetEnvironmentVariable("AZURE_CACHE_SERVICE_ENDPOINT") ?? throw new InvalidOperationException();
@@ -43,13 +43,13 @@ public class AzureCacheDocumentSearchServiceTest
         records.Count().Should().Be(3);
     }
 
-    [Xunit.Fact]
+    [EnvironmentVariablesFact("AZURE_CACHE_INDEX", "AZURE_CACHE_SERVICE_ENDPOINT", "AZURE_OPENAI_ENDPOINT", "AZURE_OPENAI_EMBEDDING_DEPLOYMENT")]
     public async Task QueryDocumentsTestEmbeddingOnlyAsync()
     {
-        Environment.SetEnvironmentVariable("AZURE_CACHE_INDEX", s_acIndex);
-        Environment.SetEnvironmentVariable("AZURE_CACHE_SERVICE_ENDPOINT", s_cacheEndpoint);
-        Environment.SetEnvironmentVariable("AZURE_OPENAI_ENDPOINT", s_openAiEndpoint);
-        Environment.SetEnvironmentVariable("AZURE_OPENAI_EMBEDDING_DEPLOYMENT", s_openAiEmbeddingDeployment);
+        //Environment.SetEnvironmentVariable("AZURE_CACHE_INDEX", s_acIndex);
+        //Environment.SetEnvironmentVariable("AZURE_CACHE_SERVICE_ENDPOINT", s_cacheEndpoint);
+        //Environment.SetEnvironmentVariable("AZURE_OPENAI_ENDPOINT", s_openAiEndpoint);
+        //Environment.SetEnvironmentVariable("AZURE_OPENAI_EMBEDDING_DEPLOYMENT", s_openAiEmbeddingDeployment);
 
         var index = Environment.GetEnvironmentVariable("AZURE_CACHE_INDEX") ?? throw new InvalidOperationException();
         var endpoint = Environment.GetEnvironmentVariable("AZURE_CACHE_SERVICE_ENDPOINT") ?? throw new InvalidOperationException();
@@ -76,12 +76,12 @@ public class AzureCacheDocumentSearchServiceTest
         records.Count().Should().Be(3);
     }
 
-    [Xunit.Fact]
+    [EnvironmentVariablesFact("AZURE_CACHE_INDEX", "AZURE_CACHE_SERVICE_ENDPOINT", "AZURE_COMPUTER_VISION_ENDPOINT")]
     public async Task QueryImagesTestAsync()
     {
-        Environment.SetEnvironmentVariable("AZURE_CACHE_INDEX", s_acIndex);
-        Environment.SetEnvironmentVariable("AZURE_CACHE_SERVICE_ENDPOINT", s_cacheEndpoint);
-        Environment.SetEnvironmentVariable("AZURE_COMPUTER_VISION_ENDPOINT", s_computerVisionEndpoint);
+        //Environment.SetEnvironmentVariable("AZURE_CACHE_INDEX", s_acIndex);
+        //Environment.SetEnvironmentVariable("AZURE_CACHE_SERVICE_ENDPOINT", s_cacheEndpoint);
+        //Environment.SetEnvironmentVariable("AZURE_COMPUTER_VISION_ENDPOINT", s_computerVisionEndpoint);
 
         var index = Environment.GetEnvironmentVariable("AZURE_CACHE_INDEX") ?? throw new InvalidOperationException();
         var endpoint = Environment.GetEnvironmentVariable("AZURE_CACHE_SERVICE_ENDPOINT") ?? throw new InvalidOperationException();
