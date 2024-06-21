4c3032916386da78c5f646aac5583c3b6c394a33
Nisarga Shashidhar <45395264+NisargaShashidhar@users.noreply.github.com>
2024-06-21 13:30:27 +0000

    Changed sample question

--- app/SharedWebComponents/Components/Examples.razor.cs
+++ app/SharedWebComponents/Components/Examples.razor.cs
@@ -7,7 +7,7 @@ public sealed partial class Examples
     [Parameter, EditorRequired] public required string Message { get; set; }
     [Parameter, EditorRequired] public EventCallback<string> OnExampleClicked { get; set; }
 
-    private string WhatIsIncluded { get; } = "What is included in my Northwind Health Plus plan that is not in standard?";
+    private string WhatIsIncluded { get; } = "How often does the Quality Council meet?";
     private string WhatIsPerfReview { get; } = "What happens in a performance review?";
     private string WhatDoesPmDo { get; } = "What does a Product Manager do?";
 
